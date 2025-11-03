1d8c6daec6574b351277b7b8fcef98071558e0ad
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2025-11-03 21:05:01 +0000

    Add multiply and power methods

--- calculator.rb
+++ calculator.rb
@@ -7,11 +7,23 @@ class Calculator
     a - b
   end
 
+  def multiply(a, b)
+    a * b
+  end
+
+  def power(a, b)
+    a ** b
+  end
 end
 
 # Testing documentation, we're just checking this in rails c.
 
 calc = Calculator.new
 
-calc.add(1, 2)
-calc.subtract(10, 4)
+puts "Testing add with 1 + 2 = 3 -> #{calc.add(1, 2) == 3 }"
+puts "Testing subtract with 5 - 1 = 4 -> #{calc.subtract(5, 1) == 4 }"
+puts "Testing subtract with 6 * 7 = 42 -> #{calc.multiply(6, 7) == 42}"
+puts "Testing power with 2 ^ 3 = 8 -> #{calc.power(2, 3) == 8 }"
+calc.square(5)
+calc.cube(3)
+
